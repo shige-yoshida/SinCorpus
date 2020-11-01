4743f4afd252147d24646bae4bd1da3e97ca3263
Shigeki Yoshida <71423991+shige-yoshida@users.noreply.github.com>
2020-11-01 09:54:40 +0900

    change in tang list

--- wiki_corpus_tag.py
+++ wiki_corpus_tag.py
@@ -15,26 +15,13 @@ for line in f_tagged:
         pass
     else:
         word.extend(line.split())
-#setにして重複要素をなくす
 
-word_2 = collections.Counter(word)
-word_2 = word_2.most_common()
-word_2 = [i[0] for i in word_2]
+#重複要素を無くして頻度順に並べる
+word = collections.Counter(word)
+word = word.most_common()
+#worから頻度を抜く
+word = [i[0] for i in word]
 
-word_1 = set(word)
-word_1 = list(word_1)
-
-word_with_tag = []
-
-
-for i in word_1:
-    flag = False
-    for j in word_2:
-        if flag == False and j.startswith(i):
-            flag = True
-            word_with_tag.append(j)  
-
-print(word_with_tag)
 f_tagged.close
 
 #WikipediaCorpusの語形をタグ付きにする
@@ -49,12 +36,12 @@ for l_wiki in f_wiki:
         flag = False
         w＿wiki_2 = w_wiki + '_'
         #wordのリスト内にあれば、タグ付きに置き換える
-        for w_tagged in word_with_tag:
+        for w_tagged in word:
             if flag == False and w_tagged.startswith(w_wiki_2):
                 flag = True
-                ind = word_with_tag.index(w_tagged)
-                #print(word_with_tag[ind])
-                wiki_tagged.append(word_with_tag[ind])
+                ind = word.index(w_tagged)
+                #print(word[ind])
+                wiki_tagged.append(word[ind])
             elif flag == False and re.search(r'^\d', w_wiki) != None:
                 flag = True
                 #print(w_wiki + '_QFNUM')
@@ -66,21 +53,22 @@ for l_wiki in f_wiki:
     #文の最後の語形について検索  
     w_wiki_last = l_wiki[-1].rstrip(".") + '_'
     flag = False
-    for w_tagged in word_with_tag:
+    for w_tagged in word:
         if flag == False and w_tagged.startswith(w_wiki_last):
             flag = True
-            ind = word_with_tag.index(w_tagged)
-            #print(word_with_tag[ind] + "\n")
-            wiki_tagged.append(word_with_tag[ind])
+            ind = word.index(w_tagged)
+            #print(word[ind] + "\n")
+            wiki_tagged.append(word[ind])
             wiki_tagged.append("\n")
     if flag == False:
         #print(l_wiki[-1] + '\n')
         wiki_tagged.append(l_wiki[-1])
         wiki_tagged.append("\n")
+
 f_wiki.close
 
-with open('sin_wikipedia_2011_100K-sentences_tagged.txt', 'w') as f_wiki_tagged:
+with open('./sin_wikipedia_2011_100K-sentences_tagged.txt', 'w') as f_wiki_tagged:
     for word_tagged in wiki_tagged:
         f_wiki_tagged.write("%s " % word_tagged)
 
-f_wiki_tagged.close 
+f_wiki_tagged.close  
